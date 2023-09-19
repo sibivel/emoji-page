93ed0d9dd1e89b5baa9c3b7aeb350466a80275c5
sibivel979@gmail.com <sibivel979@gmail.com>
2023-09-18 22:14:26 -0400

    readme + github link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,3 +4,15 @@
 
 <link rel="stylesheet" href="{base}/app.css" />
 <slot />
+<a href="https://github.com/sibivel/emoji-page" class="github-link"
+	><img src="{base}/github-icon.png" width="20px" alt="Github" /></a
+>
+
+<style>
+	.github-link {
+		position: fixed;
+		bottom: 10px; /* Adjust this value to set the vertical position from the bottom */
+		right: 10px; /* Adjust this value to set the horizontal position from the right */
+		z-index: 1000; /* Ensure it's above other content */
+	}
+</style>
