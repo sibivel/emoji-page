af9189433a7eb214da1f1bc8b074a0aa74468cee
sibivel <sibivel979@gmail.com>
2023-09-19 02:15:12 +0000

    Deploying to gh-pages from @ sibivel/emoji-page@93ed0d9dd1e89b5baa9c3b7aeb350466a80275c5 🚀

--- _app/immutable/chunks/paths.5d8ea5d8.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1xe5hhw)==null?void 0:s.base)??"/emoji-page";var e;const t=((e=globalThis.__sveltekit_1xe5hhw)==null?void 0:e.assets)??a;export{t as a,a as b};
